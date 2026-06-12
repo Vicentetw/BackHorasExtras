976e2b43f647b8cb07e30926dc7be3124920db3f
Vicentetw <75254133+Vicentetw@users.noreply.github.com>
2026-06-12 11:27:27 -0300

    agrego db.js para base de datos

---
+++ db.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql2/promise');
+
+const db = mysql.createPool({
+  host: process.env.MYSQL_ADDON_HOST,
+  user: process.env.MYSQL_ADDON_USER,
+  password: process.env.MYSQL_ADDON_PASSWORD,
+  database: process.env.MYSQL_ADDON_DB,
+  port: process.env.MYSQL_ADDON_PORT || 3306,
+  waitForConnections: true,
+  dateStrings: true,
+  connectionLimit: 10
+});
+
+module.exports = db;
